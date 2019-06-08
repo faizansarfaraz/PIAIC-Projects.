3a3691f0e7454749f957fb59054377e613001412
faizansarfaraz <51273635+faizansarfaraz@users.noreply.github.com>
2019-06-08 17:32:58 +0500

    programme to determine if number is positive, negative or zero.

---
+++ number_determination.py
@@ -0,0 +1,8 @@
+print("Programme to determine the integers")
+number = eval(input("Enter Number:"))
+if number > 0:
+    print("Positive number entered.")
+if number < 0:
+    print("Negative number entered.")
+if number == 0:
+    print("Zero entered")
